169b839eb5d557bb4fbf6f499b4cba0803023855
sunnycake <mynuex@gmail.com>
2020-10-15 16:27:47 -0500

    edit module name on api_controller. images to flickr and edemam to edamam

--- api_controller.py
+++ api_controller.py
@@ -1,14 +1,14 @@
-from api import images, edemam, spoonacular
+from api import flickr, edamam, spoonacular
 import random 
 
 
 def get_food_info(search_food, search_drink):
     
     what_to_search_for_image = random.choice([search_food, search_drink])
-    image = images.get_image(what_to_search_for_image)
+    image = flickr.get_image(what_to_search_for_image)
 
 
-    drink = edemam.get_drink(search_drink)
+    drink = edamam.get_drink(search_drink)
     recipe = spoonacular.get_recipe(search_food)
 
     return image, drink, recipe
