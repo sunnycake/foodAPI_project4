017937234e7df387908b63e111b8d54c0edf9fdb
Abdala <mtnabdala@gmail.com>
2020-10-19 11:58:54 -0500

    deleted the drink model, and added a columns to the food model

--- model_food.py
+++ model_food.py
@@ -9,13 +9,15 @@ class Food(Model):
     id = AutoField()
     food_name = CharField(unique=True)
     url = CharField()
+    drink_name = CharField(unique=True)
+    img_file_name = CharField(unique=True)
 
     class Meta():
         database = db #this model uses the "food.db" database
-        constraints = [SQL('UNIQUE( food_name COLLATE NOCASE )')]
+        constraints = [SQL('UNIQUE( food_name COLLATE NOCASE, drink_name COLLATE NOCASE )')]
 
     def __str__(self):
-        return f'{self.id}: {self.food_name}: {self.url}'
+        return f'{self.id}: {self.food_name}: {self.url}: {self.drink_name}: {self.img_file_name}'
 
 db.connect()
 db.create_tables([Food])
