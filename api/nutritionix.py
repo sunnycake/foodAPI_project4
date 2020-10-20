4fc6664261e96bbda43f281aa312d9c4a1d60143
Paul <boris.pizha@go.minneapolis.edu>
2020-10-19 20:14:53 -0500

    addinf key and id API

--- api/nutritionix.py
+++ api/nutritionix.py
@@ -8,8 +8,11 @@ app_key = os.environ.get('DRINK_KEY')
 #Aplication ID to run the program
 app_id = os.environ.get('DRINK_ID')
 
+# Id=b3203af1
+# Key=2b9b96bfe1278528cc9717ce7055a037
+
 # User will enter the drink they wanna search 
-search_drink = 'COFFEE'
+search_drink = ''
 
 #URL that will pull the drink entered by user 
 url = f'https://api.nutritionix.com/v2/search?q={search_drink}&limit=1&offset=0&appId={app_id}&appKey={app_key}'
@@ -20,7 +23,7 @@ pprint(data)
 results = data['results']
 for result in results:
     drink_name = result['item_name']
-    
+
 
 # Printing the name of drink requested by user     
 print(f'Drink name: {drink_name}')
