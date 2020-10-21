24ed6840fff48c9432e65d4d02f6afd8e3af43d4
Abdala <mtnabdala@gmail.com>
2020-10-21 14:42:20 -0500

    re-organized different functions in main to talk to different files of the program

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import peewee
 import ui
 from api import spoonacular, nutritionix, flickr
 import api_controller
@@ -28,28 +29,42 @@ def display_menu(): # Menu option for user
     print('1: Search recipe')
     print('2: Delete a recipe')
     print('3: Display all recipes')
-    print('4: Exit')
+    print('5: Exit')
 
 
 def search_recipe():
-    search_recipe = ui.get_search_term() 
+    """get info about the user's choose food and drink from 3 different api.
+        """
+    search_recipe = ui.get_non_empty_string("Please Enter recipe name? ") 
+    search_drink = ui.get_non_empty_string("Please enter drink name? ")
+    drink_name = api_controller.get_drink_info(search_drink)
+    img_file_name = api_controller.getDrinkImage(drink_name)
     recipe_name, recipe_url = api_controller.get_food_info(search_recipe)
+    save = ui.save_or_not_save()
+    if save:
+        save_record(recipe_name, recipe_url, drink_name, img_file_name)
+    ui.message("Good Bye!")
 
-    save = input('Do you want to save this recipe? Enter "y" or "n": ')
+def save_record(recipe_name, recipe_url, drink_name, img_file_name):
+    """saves a recipe record in to the database and handles for duplicate data entry"""
+    try:
+        food_record = db.create_food_record(recipe_name, recipe_url, drink_name, img_file_name)
+        food_record.save()
+    except peewee.IntegrityError:
+        ui.message(f"Failed to add! Either the Name: {recipe_name}, or the Drink: {drink_name} already exist in the database ")
 
-    if save == 'y':
-        db.save_recipe(recipe_name, recipe_url)
-    elif save == 'n':
-        print('Good bye! ')
-
-    
 def delete_recipe():
-    pass
+    get_id = ui.get_id()
+    try:
+        rows_deleted = db.delete_recipe_by_id(get_id)
+        ui.message(f"successfully deleted {rows_deleted} row")
+    except peewee.DoesNotExist:
+        ui.message("The record you are trying to delete doesn't exist")
 
 
 def display_all_recipes():
-    print("\nHere's all your recipes: \n")
-    all_recipes = db.show_all_recipes()
+    ui.message("\nHere's all your recipes: \n")
+    db.display_recipe()
 
 
 if __name__ == "__main__":
