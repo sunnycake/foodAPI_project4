809ba04468b8bc608ce02ddc22cab275e85ae7d6
sunnycake <mynuex@gmail.com>
2020-10-30 23:25:48 -0500

    add api function and additional exception

--- api/flickr.py
+++ api/flickr.py
@@ -1,31 +1,45 @@
 import requests
 import os
 from pprint import pprint
+import logging
 
-key = os.environ.get('FLICKR_KEY') # 4dd2de713ae2dfae3dc724620b1c8b92 
+key = os.environ.get('FLICKR_KEY') 
+url = 'https://www.flickr.com/services/rest'
 
 def getImage(drinkName):
-
-    url = f'https://www.flickr.com/services/rest/?method=flickr.photos.search&text={drinkName}&media=photos&per_page=1&sort=relevance&format=json&nojsoncallback=1&api_key={key}'
     try:
-        data = requests.get(url)
-        jsonFlickrApi = data.json()
-        results = jsonFlickrApi['photos']['photo']
-        for result in results:
+        query = {
+            'method': 'flickr.photos.search','text': drinkName,'media': 'photos','per_page': 1,
+            'sort': 'relevance','format': 'json','nojsoncallback': '1', 'api_key': key}
+            
+        data = flickr_api_call(query)
+        results = data['photos']['photo']
+
+        if results:
+            result = results[0]
             secret = result['secret']
             photo_id = result['id']
             server = result['server']
             title = result['title']
 
-        fetchPhotoURL = f'https://live.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg' 
-        photoResponse = requests.get(fetchPhotoURL)
+            fetchPhotoURL = f'https://live.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg' 
+            photoResponse = requests.get(fetchPhotoURL)
 
-        filename = f'{title}.jpg'
-        with open(filename, 'wb') as file:
-            for image in photoResponse.iter_content():
-                file.write(image)
-            return filename
-
+            filename = f'{title}.jpg'
+            with open(filename, 'wb') as file:
+                for image in photoResponse.iter_content():
+                    file.write(image)
+                return filename
+        else:
+            print('No results for image search. ')
+        
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
     except Exception as e:
-        print('Error with your query. ')
+        logging.error(f'Error occured calling flickr API: {e}')
+        return None, e
+
+
+def flickr_api_call(query):
+    return requests.get(url, params=query).json()
     
