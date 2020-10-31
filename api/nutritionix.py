a04f2fee4ebbc3e0c5f11d3030bb550d6d111cc5
sunnycake <mynuex@gmail.com>
2020-10-30 23:30:38 -0500

    add api function and additional exception

--- api/nutritionix.py
+++ api/nutritionix.py
@@ -1,21 +1,32 @@
 import requests 
 import os
 from pprint import pprint
+import logging
 
 app_key = os.environ.get('DRINK_KEY')
 app_id = os.environ.get('DRINK_ID')
 
+url = 'https://api.nutritionix.com/v2/search'
 
 def get_drink(search_term):
-    
-    url = f'https://api.nutritionix.com/v2/search?q={search_term}&limit=1&offset=0&appId={app_id}&appKey={app_key}'
     try:
-        data = requests.get(url).json()
+        query = {'q': search_term,'limit': 1,'offset': 0,'appId': app_id,'appKey': app_key}
+        data = nutritionix_api_call(query)
         results = data['results']
 
-        for result in results:
-            drink_name = result['item_name']
-        return drink_name
+        if results:
+            result = results[0]
+            beverage = result['item_name']
+            return beverage
+        else:
+            print('No results for drink search. ')
 
-    except:
-        print('Error with your query. ')
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    except Exception as e:
+        logging.error(f'Error occured calling drink API: {e}')
+        return None, e
+
+
+def nutritionix_api_call(query):
+    return requests.get(url, params=query).json()
