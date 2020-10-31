c6024227538d40825ac0c212589c2795a63b797f
Abdala <mtnabdala@gmail.com>
2020-10-31 15:23:21 -0500

    handle api response and improve code quality

--- api/flickr.py
+++ api/flickr.py
@@ -1,31 +1,61 @@
 import requests
 import os
 from pprint import pprint
+from http import HTTPStatus
 
-key = os.environ.get('FLICKR_KEY') # 4dd2de713ae2dfae3dc724620b1c8b92 
+key = os.environ.get('FLICKR_KEY') 
+url = 'https://www.flickr.com/services/rest/'
 
-def getImage(drinkName):
+def get_image_data(drinkName):
+    """Set up the parameters required in the search"""
+    param = {
+    'method' : 'flickr.photos.search',
+    'text' : drinkName,
+    'media': 'photos',
+    'per_page' : 1,
+    'sort' : 'relevance',
+    'format': 'json',
+    'nojsoncallback' : '1n',
+    'api_key' : key
+   }
+    """return a value if connection is successfull or error if not"""
+    try: 
+        resonse = requests.get(url, params=param)
+        data = resonse.json()
+        results = data['photos']['photo']
+        return results, None
+    except Exception as error:
+        return None, error
 
-    url = f'https://www.flickr.com/services/rest/?method=flickr.photos.search&text={drinkName}&media=photos&per_page=1&sort=relevance&format=json&nojsoncallback=1n&api_key={key}'
+def extract_image_url_params(data):
+    """get the query params from the data returned (get_image_data) function"""
     try:
-        data = requests.get(url)
-        jsonFlickrApi = data.json()
-        results = jsonFlickrApi['photos']['photo']
-        for result in results:
-            secret = result['secret']
-            photo_id = result['id']
-            server = result['server']
-            title = result['title']
+        secret = data[0]['secret']
+        photo_id = data[0]['id']
+        server = data[0]['server']
+        title = data[0]['title']
+        return server, photo_id, secret, title
+    except Exception as err:
+        return err
 
-        fetchPhotoURL = f'https://live.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg' 
+def retrieve_image(server, photo_id, secret):
+    try:
+        fetchPhotoURL = f'https://live.staticflickr.com/{server}/{photo_id}_{secret}_m.jpg' #creates img
         photoResponse = requests.get(fetchPhotoURL)
+        return photoResponse
+    except Exception as err:
+        return err
 
+def create_img_file(title, image):
+    try:
         filename = f'{title}.jpg'
         with open(filename, 'wb') as file:
-            for image in photoResponse.iter_content():
+            for image in image.iter_content():
                 file.write(image)
             return filename
+    except Exception as err:
+        return err
 
-    except Exception as e:
-        print('Error with your query. ')
-    
+
+def catch():
+    pass
--- api_controller.py
+++ api_controller.py
@@ -1,11 +1,28 @@
 
 from api import flickr, nutritionix, spoonacular
 import random 
+import logging
+from pprint import pprint
+
+def handle_flickr_api_response(drink_name):
+    image_data, error = flickr.get_image_data(drink_name)
+    if error:
+        print("Sorry! Unable to connect")
+    elif image_data:
+        server, photo_id, secret, title = flickr.extract_image_url_params(image_data)
+        img = flickr.retrieve_image(server, photo_id, secret)
+        image_file = flickr.create_img_file(title, img)
+        return image_file
+    else:
+        print("Sorry, couldn't find what you looking for. Please Try a different search ")
+
+res = handle_flickr_api_response('$$$$$$$')
+print(res)
 
 def get_food_info(search_recipe, search_drink):
     recipe_name, recipe_url = spoonacular.get_recipe(search_recipe)
     drink = nutritionix.get_drink(search_drink)
-    image = flickr.getImage(search_drink)
+    image = handle_flickr_api_response(search_drink)
 
     print("\nHere's a recipe we think you might enjoy.\n")
     print(f'Recipe name: {recipe_name}')
